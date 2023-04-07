cca86be5b654f371f269e42f473e262739b29021
Keshav Sharma <sharmakeshav15157@gmail.com>
2023-04-08 03:38:36 +0530

    set guestAuthSecret to a jwt token

--- internal/db/repositories/user_repository.go
+++ internal/db/repositories/user_repository.go
@@ -5,15 +5,17 @@ package repositories
 import (
 	"context"
 	"errors"
+	"strconv"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/TheDevExperiment/server/internal/db"
+	"github.com/TheDevExperiment/server/internal/log"
 	"github.com/TheDevExperiment/server/internal/utility/funnynamer"
+	"github.com/TheDevExperiment/server/internal/utility/jwt"
 )
 
 type UserRepository struct {
@@ -32,6 +34,19 @@ type UserModel struct {
 	Score           _score             `bson:"score,omitempty"`
 	IsActive        bool               `bson:"isActive,omitempty"`
 	DeletionReason  string             `bson:"deletionReason,omitempty"`
+}
+type UserUpdateModel struct {
+	// Id              primitive.ObjectID `bson:"_id,omitempty" `
+	IsGuest         bool               `bson:"isGuest,omitempty"`
+	GuestAuthSecret string             `bson:"guestAuthSecret,omitempty"`
+	LastModified    primitive.DateTime `bson:"lastModified,omitempty"`
+	DisplayName     string             `bson:"displayName,omitempty"`
+	// CountryID       string             `bson:"countryId,omitempty"`
+	// CityID          string             `bson:"cityId,omitempty"`
+	// Age            string `bson:"age,omitempty"`
+	Score          _score `bson:"score,omitempty"`
+	IsActive       bool   `bson:"isActive,omitempty"`
+	DeletionReason string `bson:"deletionReason,omitempty"`
 }
 
 type _score struct {
@@ -55,12 +70,10 @@ func (r *UserRepository) Find(ctx context.Context, filter interface{}) ([]UserMo
 	}
 	return results, nil
 }
-
 func (r *UserRepository) Create(ctx context.Context, userAge string, countryId string, cityId string) (UserModel, error) {
-	uuid := uuid.New().String()
 	doc := UserModel{
 		IsGuest:         true,
-		GuestAuthSecret: uuid,
+		GuestAuthSecret: "",
 		CreatedAt:       primitive.NewDateTimeFromTime(time.Now()),
 		LastModified:    primitive.NewDateTimeFromTime(time.Now()),
 		DisplayName:     funnynamer.GetFunnyName(),
@@ -71,15 +84,39 @@ func (r *UserRepository) Create(ctx context.Context, userAge string, countryId s
 		IsActive:        true,
 		DeletionReason:  "",
 	}
+
 	result, err := r.collection.InsertOne(context.Background(), doc)
 	if err != nil {
 		return UserModel{}, err
 	}
+
 	insertedID, ok := result.InsertedID.(primitive.ObjectID)
 	if !ok {
-		return UserModel{}, errors.New("Failed to convert InsertedID to ObjectID")
+		return UserModel{}, errors.New("failed to convert InsertedID to ObjectID")
 	}
+	secret, err := jwt.CreateToken(insertedID.Hex(), countryId, cityId, true)
+	if err != nil {
+		return UserModel{}, err
+	}
+
+	deltaUpdate := UserUpdateModel{
+		GuestAuthSecret: secret,
+		LastModified:    primitive.NewDateTimeFromTime(time.Now()),
+	}
+	updateResult, err := r.UpdateById(ctx, insertedID.Hex(), deltaUpdate)
+	if err != nil {
+		return UserModel{}, err
+	}
+
+	log.Debug(updateResult)
+
 	doc.Id = insertedID
+	doc.GuestAuthSecret = secret
+	doc.LastModified = primitive.NewDateTimeFromTime(time.Now())
+	doc.Score.RatingCount = 0
+	doc.Score.RatingScoreSum = 0
+	doc.IsActive = true
+	doc.DeletionReason = ""
 	return doc, nil
 }
 
@@ -88,11 +125,30 @@ func (r *UserRepository) Delete(ctx context.Context, filter interface{}) error {
 	return err
 }
 
-func (r *UserRepository) Update(ctx context.Context, filter interface{}, update interface{}) error {
+func (r *UserRepository) UpdateById(ctx context.Context, id string, update UserUpdateModel) (map[string]string, error) {
+	docId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Errorf("failed to convert id to object id: %w", err)
+		return nil, err
+	}
+
+	filter := bson.M{"_id": docId}
 	updateDoc := bson.M{
 		"$set": update,
 	}
 
-	_, err := r.collection.UpdateMany(ctx, filter, updateDoc)
-	return err
+	updateResult, err := r.collection.UpdateOne(ctx, filter, updateDoc)
+	if err != nil {
+		return nil, err
+	}
+
+	statistics := map[string]string{
+		"MatchedCount":  strconv.FormatInt(updateResult.MatchedCount, 10),
+		"ModifiedCount": strconv.FormatInt(updateResult.ModifiedCount, 10),
+		"UpsertedCount": strconv.FormatInt(updateResult.UpsertedCount, 10),
+	}
+	if updateResult.UpsertedID != nil {
+		statistics["UpsertedID"] = updateResult.UpsertedID.(primitive.ObjectID).Hex()
+	}
+	return statistics, nil
 }
---
+++ internal/utility/jwt/jwt.go
@@ -0,0 +1,69 @@
+package jwt
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/spf13/viper"
+)
+
+type Claims struct {
+	jwt.StandardClaims
+	CountryCode string
+	CityID      string
+	IsGuest     bool
+}
+
+func CreateToken(id string, countryCode string, cityId string, isGuest bool) (string, error) {
+	// Set token expiration time
+	expirationTime := time.Now().Add(time.Hour * 24).Unix()
+
+	// Create token claims
+	claims := Claims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime,
+			Issuer:    viper.GetString("jwt.issuer"),
+			Subject:   id,
+		},
+		CountryCode: countryCode,
+		CityID:      cityId,
+		IsGuest:     isGuest,
+	}
+
+	// Generate token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign token with secret
+	secret := []byte(viper.GetString("jwt.secret"))
+	signedToken, err := token.SignedString(secret)
+	if err != nil {
+		return "", fmt.Errorf("could not sign token: %w", err)
+	}
+
+	return signedToken, nil
+}
+
+func VerifyToken(tokenString string) (*Claims, error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		// Validate signing method
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		// Return secret for validation
+		return []byte(viper.GetString("jwt.secret")), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not parse token: %w", err)
+	}
+
+	// Check if token is valid
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, nil
+}
