b7fc330d99967887c14dfb93b4b0f985d1f11845
Keshav Sharma <sharmakeshav15157@gmail.com>
2023-04-08 14:47:24 +0530

    Feat/mongo client (#20)
    * wip: basic repository pattern, mongo driver setup, repositories crud functions
    
    * mongo client and removed useless function from user_repo

---
+++ internal/db/db.go
@@ -0,0 +1,45 @@
+// internal/db/db.go
+
+package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+// Connect initializes a connection to the MongoDB database
+func Connect() error {
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().
+		ApplyURI(viper.GetString("mongodb.connection_string")).
+		SetServerAPIOptions(serverAPIOptions)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	c, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	client = c
+	return nil
+}
+
+// GetClient returns the underlying mongo.Client object
+func GetClient() *mongo.Client {
+	return client
+}
+
+// GetCollection returns a mongo.Collection object for the specified collection
+func GetCollection(databaseName, collectionName string) *mongo.Collection {
+	return client.Database(databaseName).Collection(collectionName)
+}
