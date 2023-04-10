79f00574e4dd4622810af94208ca5bf63ec45413
Keshav Sharma <sharmakeshav15157@gmail.com>
2023-04-11 03:24:05 +0530

    removed AuthRequest struct

--- router/models/auth/auth.go
+++ router/models/auth/auth.go
@@ -4,10 +4,6 @@ import (
 	userModel "github.com/TheDevExperiment/server/internal/db/users/models"
 )
 
-type AuthRequest struct {
-	SecretToken string `form:"secretToken" json:"secretToken" xml:"secretToken" binding:"required"`
-}
-
 type AuthResponse struct {
 	Message string           `form:"message" json:"message" xml:"message"  binding:"required"`
 	Data    []userModel.User `form:"data" json:"data"`
